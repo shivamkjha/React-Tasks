d923654e81726d014399d45f325bad7583e6346e
Shivam Kumar Jha <shivamjhaaaa@gmail.com>
2023-10-25 23:25:40 +0530

    final commit

---
+++ wordcount/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import WordCounter from "./word_count";
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <WordCounter />
+      </div>
+    </div>
+  );
+}
+
+export default App;
